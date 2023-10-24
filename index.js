24e90240954e74684ec3ed892033cd1f32ed5e18
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2023-10-24 07:59:10 +0100

    do a basic routing

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
  const http= require("http")
+ const url=require("url")
 const server=http.createServer((request,response)=>{
-    console.log(request)
-    response.end("Hello from the server");
+    const pathName=request.url
+    if( pathName==="/" || pathName==="/overview"){
+        response.end("This is an overview page.");
+    }
+    else if(pathName==="/product"){
+        response.end("This is a product page.")
+    }
+    else{
+        response.writeHead(404,{
+        "Content-type":"text/html",
+        "my-own-header":"Hey there"
+        })
+        response.end("<h1>Page not found.</h1>")
+    }
+
 });
-server.listen(3000,"127.0.0.1",()=>{
-    console.log("Listening to request on port 3000")
+server.listen(3001,"127.0.0.1",()=>{
+    console.log("Listening to request on port 3001")
 });
+
